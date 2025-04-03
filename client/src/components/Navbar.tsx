d770f215ae68fb8b645aabdb62de19444d750108
Kiki <kirstinlisnoff5@gmail.com>
2025-04-02 18:24:55 -0600

    Added Sign Up button to landing page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -21,16 +21,21 @@ const Navbar = () => {
   return (
     <div className="display-flex justify-space-between align-center py-2 px-5 mint-green">
       <h1>
-        Authentication Review
+        Fork in the Road
       </h1>
       <div>
         {
           // Conditional rendering based on loginCheck state
           !loginCheck ? (
             // Render login button if user is not logged in
-            <button className="btn" type='button'>
-              <Link to='/login'>Login</Link>
+            <>
+            <button className="btn">
+              <Link to="/login">Log In</Link>
             </button>
+            <button className="btn">
+              <Link to="/signup">Sign Up</Link>
+            </button>
+          </>
           ) : (
             // Render logout button if user is logged in
             <button className="btn" type='button' onClick={() => {
