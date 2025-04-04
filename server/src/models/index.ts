1a24c43c6ac81709468cc0b6e54c45583d89a1cc
sigros02 <sigros02@gmail.com>
2025-04-03 22:16:25 -0400

    create vote model and vote seed

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,9 +1,25 @@
 import sequelize from "../config/connection.js";
 import { UserFactory } from "./user.js";
 import { RestaurantFactory } from "./restaurant.js";
+import { VoteFactory } from "./vote.js";
 
 const Restaurant = RestaurantFactory(sequelize);
 const User = UserFactory(sequelize);
+const Vote = VoteFactory(sequelize);
+
+Restaurant.hasMany(User, {
+  foreignKey: "RestaurantID",
+});
+User.belongsTo(Restaurant);
+
+User.hasMany(Restaurant, {
+  foreignKey: "UserID",
+});
+Restaurant.belongsTo(User);
+
+Vote.belongsTo(User, { as: "voter", foreignKey: "UserID" });
+Vote.belongsTo(Restaurant, { as: "restaurant", foreignKey: "RestaurantID" });
 
 export { Restaurant };
 export { User };
+export { Vote };
--- server/src/routes/api/restaurant-routes.ts
+++ server/src/routes/api/restaurant-routes.ts
@@ -48,42 +48,4 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-/*
-// PUT /users/:id - Update a user by id
-router.put('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { username, password } = req.body;
-  try {
-    const user = await User.findByPk(id);
-    if (user) {
-      user.username = username;
-      user.password = password;
-      await user.save();
-      res.json(user);
-    } else {
-      res.status(404).json({ message: 'User not found' });
-    }
-  } catch (error: any) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// DELETE /users/:id - Delete a user by id
-router.delete('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const user = await User.findByPk(id);
-    if (user) {
-      await user.destroy();
-      res.json({ message: 'User deleted' });
-    } else {
-      res.status(404).json({ message: 'User not found' });
-    }
-  } catch (error: any) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-*/
-
 export { router as restaurantRouter };
